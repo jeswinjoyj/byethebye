72490307b8b401b6cc2ab76275e67709fc4bf835
Jeswin Joy <80018852+jeswinjoyj@users.noreply.github.com>
2025-06-07 19:24:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    setTimeout(() => {
+      document.getElementById("h1-hi-pookies").style.borderColor = "transparent";
+    }, 4000);
+  
+    setTimeout(() => {
+      document.getElementById("h2-say").style.borderColor = "transparent";
+    }, 9000);
+  
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /android|iphone|ipad|ipod|mobile/i.test(userAgent);
+  
+    if (!isMobile) {
+      document.getElementById("h3-laptop").style.opacity = 1;
+    }  
+  };
+  
