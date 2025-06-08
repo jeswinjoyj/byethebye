b1dcef849549f4cb07e697fa45afbd45ce48b1fa
Jeswin Joy <80018852+jeswinjoyj@users.noreply.github.com>
2025-06-07 20:30:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ window.onload = function () {
     const userAgent = navigator.userAgent.toLowerCase();
     const isMobile = /android|iphone|ipad|ipod|mobile/i.test(userAgent);
   
-    if (isMobile) {
-        setTimeout(() => {
-            document.getElementById("laptop").style.opacity = 1;
-            document.getElementById("h3-laptop").style.opacity = 1;
-        }, 8000);
-    }  
+    // Get the log <p> element
+    const logElement = document.getElementById("log-output");
+  
+    if (logElement) {
+      logElement.innerHTML += `<strong>userAgent:</strong> ${userAgent}<br>`;
+      logElement.innerHTML += `<strong>isMobile:</strong> ${isMobile}<br>`;
+    }
+  
+    if (!isMobile) {
+      setTimeout(() => {
+        document.getElementById("laptop").style.opacity = 1;
+        document.getElementById("h3-laptop").style.opacity = 1;
+      }, 8000);
+    }
   };
+  
+  
   
